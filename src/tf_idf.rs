4e7996d5720c4785923dc87589e17651d1061ab9
Afonso Barracha <barracha.afonso@gmail.com>
2023-04-16 21:48:29 +0100

    chore: update license header

--- src/tf_idf.rs
+++ src/tf_idf.rs
@@ -1,19 +1,17 @@
 // Copyright (C) 2023 Afonso Barracha
 //
-// This file is part of keyword-extraction.
-//
-// keyword-extraction is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
+// Rust Keyword Extraction is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 //
-// keyword-extraction is distributed in the hope that it will be useful,
+// Rust Keyword Extraction is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
+// GNU Lesser General Public License for more details.
 //
-// You should have received a copy of the GNU General Public License
-// along with keyword-extraction.  If not, see <http://www.gnu.org/licenses/>.
+// You should have received a copy of the GNU Lesser General Public License
+// along with Rust Keyword Extraction. If not, see <http://www.gnu.org/licenses/>.
 
 use std::cmp::Ordering;
 use std::collections::{HashMap, HashSet};
