9d03363c79b8aaa1cc959808750d54d3c57f4b77
Afonso Barracha <barracha.afonso@gmail.com>
2023-05-19 12:20:48 +0100

    fix(simplified_yake): add cooccurence avg

--- src/simplified_yake/calculate_weights.rs
+++ src/simplified_yake/calculate_weights.rs
@@ -15,12 +15,15 @@
 
 use std::collections::HashMap;
 
-pub struct WeightedCandidate<'a> {
-    term: &'a str,
+use crate::co_occurrence::CoOccurrence;
+
+pub struct WeightedCandidate {
+    term: String,
     tf: f32,
     c_value: f32,
     pfo: f32,
     plo: f32,
+    avg_cooccurrence: f32,
 }
 
 fn generate_count_hashmap<'a>(candidates: &'a [String]) -> HashMap<&'a str, f32> {
@@ -71,19 +74,44 @@ fn calculate_plo<'a>(counts: &HashMap<&'a str, f32>) -> HashMap<&'a str, f32> {
         .collect::<HashMap<&'a str, f32>>()
 }
 
-impl<'a> WeightedCandidate<'a> {
-    pub fn new(term: &'a str, tf: f32, c_value: f32, pfo: f32, plo: f32) -> WeightedCandidate<'a> {
+fn calculate_avg_cooccurrence<'a>(
+    co_occurrence_matrix: CoOccurrence,
+    candidates: &'a [String],
+) -> HashMap<&'a str, f32> {
+    let mut avg_cooccurrence = HashMap::new();
+
+    for candidate in candidates {
+        if let Some(cooccurrence_row) = co_occurrence_matrix.get_matrix_row(candidate) {
+            let sum: f32 = cooccurrence_row.iter().sum();
+            let avg = sum / cooccurrence_row.len() as f32;
+            avg_cooccurrence.insert(candidate.as_str(), avg);
+        }
+    }
+
+    avg_cooccurrence
+}
+
+impl WeightedCandidate {
+    pub fn new(
+        term: String,
+        tf: f32,
+        c_value: f32,
+        pfo: f32,
+        plo: f32,
+        avg_cooccurrence: f32,
+    ) -> WeightedCandidate {
         WeightedCandidate {
             term,
             tf,
             c_value,
             pfo,
             plo,
+            avg_cooccurrence,
         }
     }
 
-    pub fn term(&self) -> &'a str {
-        self.term
+    pub fn term(&self) -> String {
+        self.term.to_owned()
     }
 
     pub fn tf(&self) -> f32 {
@@ -101,9 +129,17 @@ impl<'a> WeightedCandidate<'a> {
     pub fn plo(&self) -> f32 {
         self.plo
     }
+
+    pub fn avg_cooccurrence(&self) -> f32 {
+        self.avg_cooccurrence
+    }
 }
 
-pub fn calculate_weights(candidates: &[String]) -> Vec<WeightedCandidate> {
+pub fn calculate_weights(
+    sentences: &[String],
+    candidates: &[String],
+    window_size: usize,
+) -> Vec<WeightedCandidate> {
     let total_terms = candidates.len() as f32;
 
     if total_terms == 0.0 {
@@ -111,10 +147,13 @@ pub fn calculate_weights(candidates: &[String]) -> Vec<WeightedCandidate> {
     }
 
     let counts = generate_count_hashmap(candidates);
+    let co_occurrence_matrix = CoOccurrence::new(sentences, candidates, window_size);
+
     let tf = calculate_tf(&counts);
     let c_value = calculate_c_value(&tf);
     let pfo = calculate_pfo(&counts, total_terms);
     let plo = calculate_plo(&counts);
+    let avg_cooccurrence = calculate_avg_cooccurrence(co_occurrence_matrix, candidates);
 
     candidates
         .iter()
@@ -123,8 +162,26 @@ pub fn calculate_weights(candidates: &[String]) -> Vec<WeightedCandidate> {
             let c_value = c_value.get(candidate.as_str()).unwrap_or(&0.0);
             let pfo = pfo.get(candidate.as_str()).unwrap_or(&0.0);
             let plo = plo.get(candidate.as_str()).unwrap_or(&0.0);
+            let avg_cooccurrence = avg_cooccurrence.get(candidate.as_str()).unwrap_or(&0.0);
 
-            WeightedCandidate::new(candidate.as_str(), *tf, *c_value, *pfo, *plo)
+            WeightedCandidate::new(
+                candidate.clone(),
+                *tf,
+                *c_value,
+                *pfo,
+                *plo,
+                *avg_cooccurrence,
+            )
         })
         .collect::<Vec<WeightedCandidate>>()
+}
+
+impl WeightedCandidate {
+    pub fn calculate_score(&self, w_tf: f32, w_c: f32, w_pf: f32, w_pl: f32, w_avg: f32) -> f32 {
+        self.tf.powf(w_tf)
+            * self.c_value.powf(w_c)
+            * self.pfo.powf(w_pf)
+            * self.plo.powf(w_pl)
+            * self.avg_cooccurrence.powf(w_avg)
+    }
 }
